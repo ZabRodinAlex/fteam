cb9e10da33fd248efe78a70fec60a10be1d5f0d2
ZabRodinAlex <79101444198@ya.ru>
2015-12-24 22:01:13 +0400

    add search

--- index.php
+++ index.php
@@ -2,19 +2,25 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Футбольные клубы</title>
-	<script type="text/javascript" src="/files/js/jQuery.js"></script>
-	<link rel="stylesheet" href="/files/css/style.css">
+	<script type="text/javascript" src="/files/jQuery.js"></script>
+        <script type="text/javascript" src="/files/chart.js"></script>
+	<link rel="stylesheet" href="/files/style.css">
 </head>
 <body>
 <div id="wrapper">
 	<div class="content">
 		<div class="header">	
 			<h2><a class="logo" href="http://rfpl.org/clubs" target="_blank">Футбольные клубы</a></h2>
+			<div class="searchform">	
+				<input class="searchfield" type="text" placeholder="фамилия футболиста"/>
+				<input class="searchbutton" type="submit" value="искать" />
+			</div>
 		</div>
 		<div class="tables">
 			<table id="listofclubs"></table>
 			<table id="contentsofclub"></table>
 		</div>
+		<canvas id="charts" style="width:300; height:300; margin:0 0 0 38%;"></canvas>
 	</div>
 </div>
 
@@ -25,6 +31,17 @@
 			success: function(html){ $("#listofclubs").html(html);},
 		});
 	}, 1000);
+	$('.searchbutton').click( function () {
+		var searchdata = $('.searchfield').val();
+		if ( searchdata ) {
+			$.ajax({
+				type: "POST",
+				url: "search.php",
+				data: "searchdata=" + searchdata,
+				success: function(html){ $("#contentsofclub").html(html);},
+			});
+		}
+	});
 </script>
 </body>
 </html>
